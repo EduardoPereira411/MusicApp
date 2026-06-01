b74f01296aa1aa0e071b95924f072b4bf16f2ad8
Eduardo Pereira <eduardocastropereira@gmail.com>
2026-06-01 22:11:57 +0100

    Changed _layout so the media control logs are ignored

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,23 @@
 import { Stack } from "expo-router";
+import { LogBox } from "react-native";
+// Ignore Media COntrol Logs
+LogBox.ignoreLogs(["Dispatching media control event"]);
+const originalLog = console.log;
+console.log = (...args) => {
+  try {
+    const logString = args
+      .map((arg) =>
+        typeof arg === "object" ? JSON.stringify(arg) : String(arg),
+      )
+      .join(" ");
+
+    if (logString.includes("📱 JS")) {
+      return;
+    }
+  } catch (e) {}
+
+  originalLog(...args);
+};
 
 export default function RootLayout() {
   return (
